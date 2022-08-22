a47113cc2af0d7f780f5bbd8c0bdfbc13a2c880c
zhouzhou9547 <111735687+zhouzhou9547@users.noreply.github.com>
2022-08-22 17:31:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][2]
-  return weather['weather'], math.floor(weather['temp'])
+  return weather['weather'], math.floor(weather['temp']['humidity']['wind'] )
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
